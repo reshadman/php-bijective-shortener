5171d67ac6bda3502de18cebd2239a95e24fd291
Reza Shadman <pcfeeler@gmail.com>
2015-03-15 12:16:42 +0330

    Initilize and add tests

---
+++ tests/BijectiveShortenerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Reshadman\BijectiveShortener\BijectiveShortener;
+
+class BijectiveShortenerTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test pack shortener create from integer method
+	 *
+	 * @return void
+	 */
+	public function testPackShortenerCreateFromInteger()
+	{
+		ini_set('memory_limit', -1);
+
+		$coded = [];
+
+		for ($i = 5000000; $i <= 5001000; $i++)
+		{
+			$encoded = BijectiveShortener::makeFromInteger($i);
+
+			$this->assertArrayNotHasKey($encoded, $coded);
+
+			$this->assertEquals($i, BijectiveShortener::decodeToInteger($encoded));
+
+			$coded[$encoded] = $i;
+		}
+	}
+}
